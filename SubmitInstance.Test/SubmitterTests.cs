a4b8ed014c6cc589b92554d8620488c9ba948237
Burak Dilşen <bdilsen@gmail.com>
2022-07-05 15:53:28 +0300

    unit test project initialization for SubmitInstance()

---
+++ SubmitInstance.Test/SubmitterTests.cs
@@ -0,0 +1,23 @@
+namespace SubmitInstance.Test
+{
+	public class SubmitterTests
+	{
+		[Fact]
+		public void SubmitRequest_BuildBody_RequestBodyIsTest()
+		{
+			//Arrange
+
+
+			//Act
+
+
+			//Assert
+		}
+
+		[Fact]
+		public void SubmitRequest_BuildBody_RequestBodyIsProd()
+		{
+
+		}
+	}
+}
