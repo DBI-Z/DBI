98f54138be940bc175deeb9ff593f259a46762f7
Burak Dilşen <bdilsen@gmail.com>
2022-09-28 19:24:56 +0300

    relative/absolute path for input xml

--- VB/SubmitInstance/InstanceReader.vb
+++ VB/SubmitInstance/InstanceReader.vb
@@ -4,8 +4,15 @@ Friend Class InstanceReader
 	Implements IInstanceReader
 
 	Public Function Read(xmlFileName As String) As String Implements IInstanceReader.Read
-		If File.Exists(xmlFileName) Then
-			Dim r As StreamReader = New StreamReader(xmlFileName)
+		Dim absolutePath As String
+		If Path.IsPathRooted(xmlFileName) Then
+			absolutePath = xmlFileName
+		Else
+			absolutePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, xmlFileName)
+		End If
+
+		If File.Exists(absolutePath) Then
+			Dim r As StreamReader = New StreamReader(absolutePath)
 			Return r.ReadToEnd()
 		Else
 			Return String.Empty
